51c7cbf0bcd781d03102606f7551dea279401f74
Edoardo Mistretta <privato.edoardomistretta@gmail.com>
2025-09-21 01:46:50 +0200

    implement config_flow

--- custom_components/sabiana_hvac/const.py
+++ custom_components/sabiana_hvac/const.py
@@ -1,2 +1,12 @@
+DOMAIN = "sabiana_hvac"
+
 BASE_URL = "https://be-standard.sabianawm.cloud"
 USER_AGENT = "Mozilla/5.0 (Linux; Android 11; IN2013) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Mobile Safari/537.36"
+
+ERROR_INVALID_AUTH = "invalid_auth"
+ERROR_CANNOT_CONNECT = "cannot_connect"
+ERROR_TIMEOUT = "timeout_error"
+ERROR_API_ERROR = "api_error"
+ERROR_UNKNOWN = "unknown_error"
+
+CONF_TOKEN = "token"
